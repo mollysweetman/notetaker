14e9a25bf01f3dbea14c0f0631629a1e3410782e
mollysweetman <67077842+mollysweetman@users.noreply.github.com>
2020-10-15 14:34:10 -0600

    capeescape Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 // Require Dependencies
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+console.log('hello');
+
+
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log('app running on port: ' + PORT);
+});
